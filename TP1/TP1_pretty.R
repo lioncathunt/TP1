f208122871850c6d589068b224455244deeb9614
scecchetti <scecchetti2001@gmail.com>
2026-04-14 11:57:30 -0300

    added seminario en onenote de sofi, added plot of logs and recalculated k

--- TP1/TP1_pretty.R
+++ TP1/TP1_pretty.R
@@ -118,11 +118,55 @@ print(coef(lm_NaOH_vs_k_m1)[2])
 
 #the value of k is then 0.0154 L*mol^-1*s^-1 =0.9268 L*mol^-1*min^-1 
 
+#what happens if we get the value of m and the value of k from applying logs
+#to both sides of k'=k[OH]^m
+#we get ln(k')=ln(k)+m*ln([OH-]) then if we plot ln(k')vsln([OH-]) we get
 
+Data_TP1_NaOH_K_with_logs<-Data_TP1_NaOH_K %>% 
+  mutate(logarithm_of_k_prime=log(K.prime.value)) %>% 
+  mutate(logarithm_of_NaOH=log(Concentration.of.NaOH))
+
+ggplot(data = Data_TP1_NaOH_K_with_logs,
+       aes(x=logarithm_of_NaOH,
+           y=logarithm_of_k_prime))+
+  geom_point()+
+  geom_smooth(method = "lm")+
+  stat_regline_equation()
+lm_plot_logs<-lm(Data_TP1_NaOH_K_with_logs$logarithm_of_k_prime~Data_TP1_NaOH_K_with_logs$logarithm_of_NaOH)
+summary(lm_plot_logs)
+
+#intercept from this lm=ln(k)=
+print(coef(lm_plot_logs)[1])
+#slope from this lm=m=
+print(coef(lm_plot_logs)[2])
+
+#then k in L*mol-1*min-1=
+print((exp(coef(lm_plot_logs)[1]))*60)
+
+#m is equal to the slope = 1.23863
 #Guia de Preguntas y Discusion
 #1. ¿Por que la reaccion se considera de pseudo-primer orden respecto a la fenolftaleına?
+  # Se considera de pseudo-primer orden respecto a la fenolftalina 
+  # porque la concentracion de NaOH es tan grande en comparación que se puede tomar como constante.
+
+
 #2. ¿Que efecto tiene la fuerza ionica constante en los resultados? Explicar en terminos de interacciones ionicas.
+
+  # La fuerza iónica es importante para que la concentración de NaOH para asegurarnos que las interacciones iónicas 
+  # entre las diferentes concentración influyan en la misma medida.
+
 #3. ¿Por que es importante retirar la cubeta del espectrofotometro entre mediciones?
+
+  #
+
 #4. Comparar el valor de k obtenido con el reportado en la literatura. Analizar posibles fuentes de error.
+
+  # Nos dió 0.9268 L.mol-1min-1 m mientras que en la literatura es de 1.1 L.mol−1.min−1. 
+  # Las fuentes de error pudieron haber sido las preparaciones de las soluciones de NaOH y NaCl 
+  # pudiendo generarse errores de pesada. 
+  # 
+
 #5. Proponga un mecanismo de reaccion coherente con los ordenes determinados.
+
+  #
                       
