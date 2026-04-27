82893e64a5a38dfcdb0fac465208a60677b9e9f6
lioncathunt <leonhalleresc@gmail.com>
2026-04-27 00:02:13 -0300

    added orca results for the enhanced version of 180 and 135 angles. calculated the transition state for 180 and 135 added the sections to R script

--- TP3/Trabajo practico 3.R
+++ TP3/Trabajo practico 3.R
@@ -18,12 +18,12 @@ View(output_HHH90_orca_TP3)
 # Grafico 180 -------------------------------------------------------------
 
 z_matrix180 <- acast(output_HHH180_orca_TP3, dH1H2 ~ dH1H3, value.var = "Eh")
-d_h1h2 <- as.numeric(colnames(z_matrix180))
-d_h1h3 <- as.numeric(rownames(z_matrix180))
+d_h1h2_180 <- as.numeric(colnames(z_matrix180))
+d_h1h3_180 <- as.numeric(rownames(z_matrix180))
 
 fig_180_contour <- plot_ly(
-  x = ~d_h1h2,
-  y = ~d_h1h3,
+  x = ~d_h1h2_180,
+  y = ~d_h1h3_180,
   z = ~z_matrix180,
   type = "contour",
   colorscale = "Jet",
@@ -38,7 +38,7 @@ fig_180_contour <- plot_ly(
   )
 fig_180_contour
 
-fig_180 <- plot_ly(x = ~d_h1h2, y = ~d_h1h3, z = ~z_matrix180) %>%
+fig_180 <- plot_ly(x = ~d_h1h2_180, y = ~d_h1h3_180, z = ~z_matrix180) %>%
   add_surface(
     colorscale = "Jet",
     colorbar = list(title = "Energ´ıa (E)")
@@ -54,17 +54,101 @@ fig_180 <- plot_ly(x = ~d_h1h2, y = ~d_h1h3, z = ~z_matrix180) %>%
 fig_180
 
 
+# Grafico 180 enhanced ----------------------------------------------------
 
+library(readxl)
+Resultados_orca_HHH180_enhanced_TP3 <- read_excel("Resultados orca HHH180 enhanced/Resultados orca HHH180 enhanced.xlsx")
+View(Resultados_orca_HHH180_enhanced)
+
+z_matrix180_enhanced <- acast(Resultados_orca_HHH180_enhanced_TP3, dh1h2 ~ dh1h3, value.var = "eh")
+d_h1h2_180_enhanced <- as.numeric(colnames(z_matrix180_enhanced))
+d_h1h3_180_enhanced <- as.numeric(rownames(z_matrix180_enhanced))
+
+fig_180_enhanced_contour <- plot_ly(
+  x = ~d_h1h2_180_enhanced,
+  y = ~d_h1h3_180_enhanced,
+  z = ~z_matrix180_enhanced,
+  type = "contour",
+  colorscale = "Jet",
+  autocontour = TRUE,
+  line = list(smoothing = 0.85, color ="white", width = 0.5)
+) %>%
+  layout(
+    title = "Mapa de Contornos de Energia Potencial",
+    xaxis = list(title = "R(H-H, ˚A)"),
+    yaxis = list(title = "R(H-H, ˚A)"),
+    plot_bgcolor = "#e5ecf6"
+  )
+fig_180_enhanced_contour
+
+fig_180_enhanced <- plot_ly(x = ~d_h1h2_180_enhanced, y = ~d_h1h3_180_enhanced, z = ~z_matrix180_enhanced) %>%
+  add_surface(
+    colorscale = "Jet",
+    colorbar = list(title = "Energıa (E)")
+  ) %>%
+  layout(
+    title = "SEP (180°) H + H-H -> H-H + H",
+    scene = list(
+      xaxis = list(title = "R(H-H, ˚A)"),
+      yaxis = list(title = "R(H-H, ˚A)"),
+      zaxis = list(title = "Energ´ıa (E)")
+    )
+  )
+fig_180_enhanced
+
+
+# Grafico 180 enhanced 0.1 Angstrom ---------------------------------------
+Output_180_enhanced_0_1 <- read_excel("Resultados orca HHH180 enhanced 0.1/Output_180_enhanced_0.1.xlsx")
+View(Output_180_enhanced_0_1)
+
+z_matrix180_enhanced_0.1 <- acast(Output_180_enhanced_0_1, dH1H2 ~ dH1H3, value.var = "Eh")
+d_h1h2_180_enhanced_0.1 <- as.numeric(colnames(z_matrix180_enhanced_0.1))
+d_h1h3_180_enhanced_0.1 <- as.numeric(rownames(z_matrix180_enhanced_0.1))
+
+fig_180_enhanced_0.1_contour <- plot_ly(
+  x = ~d_h1h2_180_enhanced_0.1,
+  y = ~d_h1h3_180_enhanced_0.1,
+  z = ~z_matrix180_enhanced_0.1,
+  type = "contour",
+  colorscale = "Jet",
+  autocontour = TRUE,
+  line = list(smoothing = 0.85, color ="white", width = 0.5)
+) %>%
+  layout(
+    title = "Mapa de Contornos de Energia Potencial",
+    xaxis = list(title = "R(H-H, ˚A)"),
+    yaxis = list(title = "R(H-H, ˚A)"),
+    plot_bgcolor = "#e5ecf6"
+  )
+fig_180_enhanced_0.1_contour
+
+fig_180_enhanced_0.1 <- plot_ly(x = ~d_h1h2_180_enhanced_0.1, y = ~d_h1h3_180_enhanced_0.1, z = ~z_matrix180_enhanced_0.1) %>%
+  add_surface(
+    colorscale = "Jet",
+    colorbar = list(title = "Energıa (E)")
+  ) %>%
+  layout(
+    title = "SEP (180°) H + H-H -> H-H + H",
+    scene = list(
+      xaxis = list(title = "R(H-H, ˚A)"),
+      yaxis = list(title = "R(H-H, ˚A)"),
+      zaxis = list(title = "Energ´ıa (E)")
+    )
+  )
+fig_180_enhanced_0.1
+
+
+#El estado de transicion se encuentra en x=0.9305556, y=0.9305556 y tiene una energia de -1.596486. Con una resolucion de 0.01Angstrom
 # Grafico 135 -------------------------------------------------------------
 
 
 z_matrix135 <- acast(output_HHH135_orca_TP3, dH1H2 ~ dH1H3, value.var = "Eh")
-d_h1h2 <- as.numeric(colnames(z_matrix135))
-d_h1h3 <- as.numeric(rownames(z_matrix135))
+d_h1h2_135 <- as.numeric(colnames(z_matrix135))
+d_h1h3_135 <- as.numeric(rownames(z_matrix135))
 
 fig_135_contour <- plot_ly(
-  x = ~d_h1h2,
-  y = ~d_h1h3,
+  x = ~d_h1h2_135,
+  y = ~d_h1h3_135,
   z = ~z_matrix135,
   type = "contour",
   colorscale = "Jet",
@@ -79,7 +163,7 @@ fig_135_contour <- plot_ly(
   )
 fig_135_contour
 
-fig_135 <- plot_ly(x = ~d_h1h2, y = ~d_h1h3, z = ~z_matrix135) %>%
+fig_135 <- plot_ly(x = ~d_h1h2_135, y = ~d_h1h3_135, z = ~z_matrix135) %>%
   add_surface(
     colorscale = "Jet",
     colorbar = list(title = "Energıa (E)")
@@ -95,7 +179,133 @@ fig_135 <- plot_ly(x = ~d_h1h2, y = ~d_h1h3, z = ~z_matrix135) %>%
 fig_135
 
 
+# Grafico 135 enhanced ----------------------------------------------------
+Output_HHH135_enhanced <- read_excel("Resultados orca HHH135 enhanced/Output_HHH135_enhanced.xlsx", 
+                                     col_types = c("numeric", "numeric", "numeric"))
+View(Output_HHH135_enhanced)
+
+z_matrix135_enhanced <- acast(Output_HHH135_enhanced, dH1H2 ~ dH1H3, value.var = "Eh")
+d_h1h2_135_enhanced <- as.numeric(colnames(z_matrix135_enhanced))
+d_h1h3_135_enhanced <- as.numeric(rownames(z_matrix135_enhanced))
+
+fig_135_enhanced_contour <- plot_ly(
+  x = ~d_h1h2_135_enhanced,
+  y = ~d_h1h3_135_enhanced,
+  z = ~z_matrix135_enhanced,
+  type = "contour",
+  colorscale = "Jet",
+  autocontour = TRUE,
+  line = list(smoothing = 0.85, color ="white", width = 0.5)
+) %>%
+  layout(
+    title = "Mapa de Contornos de Energıa Potencial",
+    xaxis = list(title = "R(H-H, ˚A)"),
+    yaxis = list(title = "R(H-H, ˚A)"),
+    plot_bgcolor = "#e5ecf6"
+  )
+fig_135_enhanced_contour
+
+fig_135_enhanced <- plot_ly(x = ~d_h1h2_135_enhanced, y = ~d_h1h3_135_enhanced, z = ~z_matrix135_enhanced) %>%
+  add_surface(
+    colorscale = "Jet",
+    colorbar = list(title = "Energıa (E)")
+  ) %>%
+  layout(
+    title = "SEP (135°) H + H-H -> H-H + H",
+    scene = list(
+      xaxis = list(title = "R(H-H, ˚A)"),
+      yaxis = list(title = "R(H-H, ˚A)"),
+      zaxis = list(title = "Energıa (E)")
+    )
+  )
+fig_135_enhanced
+
+
+# Grafico 135 enhanced 0.1 Angstrom ---------------------------------------
+
+Output_HHH135_enhanced_0.1 <- read_excel("Resultados orca HHH135 enhanced 0.1/Output_HHH135_enhanced_0.1.xlsx", 
+                                         col_types = c("numeric", "numeric", "numeric"))
+View(Output_HHH135_enhanced_0.1)
+
+z_matrix135_enhanced_0.1 <- acast(Output_HHH135_enhanced_0.1, dH1H2 ~ dH1H3, value.var = "Eh")
+d_h1h2_135_enhanced_0.1 <- as.numeric(colnames(z_matrix135_enhanced_0.1))
+d_h1h3_135_enhanced_0.1 <- as.numeric(rownames(z_matrix135_enhanced_0.1))
+
+fig_135_enhanced_0.1_contour <- plot_ly(
+  x = ~d_h1h2_135_enhanced_0.1,
+  y = ~d_h1h3_135_enhanced_0.1,
+  z = ~z_matrix135_enhanced_0.1,
+  type = "contour",
+  colorscale = "Jet",
+  autocontour = TRUE,
+  line = list(smoothing = 0.85, color ="white", width = 0.5)
+) %>%
+  layout(
+    title = "Mapa de Contornos de Energıa Potencial",
+    xaxis = list(title = "R(H-H, ˚A)"),
+    yaxis = list(title = "R(H-H, ˚A)"),
+    plot_bgcolor = "#e5ecf6"
+  )
+fig_135_enhanced_0.1_contour
+
+fig_135_enhanced_0.1 <- plot_ly(x = ~d_h1h2_135_enhanced_0.1, y = ~d_h1h3_135_enhanced_0.1, z = ~z_matrix135_enhanced_0.1) %>%
+  add_surface(
+    colorscale = "Jet",
+    colorbar = list(title = "Energıa (E)")
+  ) %>%
+  layout(
+    title = "SEP (135°) H + H-H -> H-H + H",
+    scene = list(
+      xaxis = list(title = "R(H-H, ˚A)"),
+      yaxis = list(title = "R(H-H, ˚A)"),
+      zaxis = list(title = "Energıa (E)")
+    )
+  )
+fig_135_enhanced_0.1
+
 # Grafico 90 --------------------------------------------------------------
 
 z_matrix90<-acast(output_HHH90_orca_TP3, dH1H2 ~ dH1H3, value.var = "Eh")
-d_
+d_h1h2_90<-as.numeric(colnames(z_matrix90))
+d_h1h3_90<-as.numeric(rownames(z_matrix90))
+
+fig_contour_90<-plot_ly(
+  x= ~d_h1h2_90,
+  y= ~d_h1h3_90,
+  z= ~z_matrix90,
+  type = "contour",
+  color = "jet",
+  autocontour= T,
+  line= list(smoothing=0.85, colour="white", width=0.5)
+) %>% 
+  layout(
+    title="SEP (90°) H + H-H -> H-H + H",
+    scene = list(
+      xaxis = list(title = "R(H-H, ˚A)"),
+      yaxis = list(title = "R(H-H, ˚A)"),
+      zaxis = list(title = "Energıa (E)")
+    )
+  )
+
+fig_contour_90
+
+fig_90<-plot_ly(x=~d_h1h2_90,
+                y=~d_h1h3_90,
+                z=~z_matrix90) %>% 
+  add_surface(
+    colorscale="jet",
+    colorbar = list(title = "Energıa (E)")
+  ) %>%
+  layout(
+    title = "SEP (90°) H + H-H -> H-H + H",
+    scene = list(
+      xaxis = list(title = "R(H-H, ˚A)"),
+      yaxis = list(title = "R(H-H, ˚A)"),
+      zaxis = list(title = "Energıa (E)")
+    )
+  )
+fig_90
+
+
+#minimum barrier 180°:-1.594396  135°: -1.588611  90°: -1.558607
+
