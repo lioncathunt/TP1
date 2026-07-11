38f5d738e950e0de1d1c51a543aac7bf7fa6760f
lioncathunt <leonhalleresc@gmail.com>
2026-07-11 14:22:29 -0300

    tablas script

--- Cosas de oceanografia (me dio pereza hacer un git nuevo)/TP FInal.R
+++ Cosas de oceanografia (me dio pereza hacer un git nuevo)/TP FInal.R
@@ -99,3 +99,46 @@ Curva_calibracion_silicio<-ggplot(subset(Cuantificacion_nutrientes_bonito_xlsx_c
 
 
 Curva_calibracion_silicio
+
+
+
+# csvs --------------------------------------------------------------------
+
+data_nitritos<-Cuantificacion_nutrientes_bonito_xlsx_tidy_data |> 
+  select("Muestra", "nitrito(uM)","amonio(uM)") |> 
+  drop_na("nitrito(uM)")
+View(data_nitritos)
+
+write.csv(data_nitritos, "Data_nitritos_amonio.csv")
+
+
+data_fosforo<-Cuantificacion_nutrientes_bonito_xlsx_tidy_data |> 
+  select("Muestra", "fosforo(uM)") |> 
+  drop_na("fosforo(uM)")
+View(data_fosforo)
+
+write.csv(data_fosforo, "data tablas/Data_fosforo.csv")
+
+
+data_silicio<-Cuantificacion_nutrientes_bonito_xlsx_tidy_data |> 
+  select("Muestra", "silicio(uM)") |> 
+  drop_na("silicio(uM)")
+View(data_silicio)
+
+write.csv(data_silicio, "data tablas/Data_silicio.csv")
+
+
+data_Chlorophil_a_b_c<-Cuantificacion_nutrientes_bonito_xlsx_tidy_data |> 
+  select("Muestra", "Chl_a(mg/L)","Chl_b(mg/L)","Chl_c(mg/L)") |> 
+  drop_na("Chl_a(mg/L)")
+View(data_Chlorophil_a_b_c)
+write.csv(data_Chlorophil_a_b_c, "data tablas/Data_Chlorophil_a_b_c.csv")
+
+
+
+
+data_Chlorophil_a_lor<-Cuantificacion_nutrientes_bonito_xlsx_tidy_data |> 
+  select("Muestra", "Chl_a_lor(mg/L)","Feofitina(mg/L)") |> 
+  drop_na("Chl_a_lor(mg/L)")
+View(data_Chlorophil_a_lor)
+write.csv(data_Chlorophil_a_lor, "data tablas/Data_Chlorophil_a_lor.csv")
